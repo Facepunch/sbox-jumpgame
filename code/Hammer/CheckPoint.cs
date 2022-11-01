89d96a259827227221c0cfa933c21717c6d8db03
bakscratch <lt_8@hotmail.co.uk>
2022-11-01 00:35:34 +0000

    Added wip checkpoint

---
+++ code/Hammer/CheckPoint.cs
@@ -0,0 +1,55 @@
+﻿
+
+using Sandbox;
+using SandboxEditor;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+/// <summary>
+/// A simple trigger volume that fires once and then removes itself.
+/// </summary>
+[Library( "js_landmark" )]
+[AutoApplyMaterial( "materials/editor/landmark/landmark.vmat" )]
+[Solid]
+[Display( Name = "Landmark", GroupName = "Jumper", Description = "Landmark" ), Category( "Triggers" ), Icon( "landscape" )]
+[HammerEntity]
+public partial class CheckPoint : TriggerMultiple
+{
+
+	[Property( "landmarkname", Title = "Land" )]
+	public string LandMarkName { get; set; } = "";
+
+	public string LandMarkMessage;
+	public string UpperCase;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		UpperCase = LandMarkName.ToUpper();
+		EnableTouchPersists = true;
+
+	}
+
+	[ClientRpc]
+	public static void NewCheckPoint( string Title )
+	{
+		JumperCheckPoint.ShowCheckPoint( Title );
+	}
+
+	public override void OnTouchStart( Entity other )
+	{
+		base.OnTouchStart( other );
+
+		if ( !other.IsServer ) return;
+		if ( other is not JumperPawn pl ) return;
+
+		if ( other != null )
+		{
+			NewCheckPoint( To.Single( other ), $"---ENTERING {UpperCase}---" );
+
+			//Should use this as a place to save the players progress so they can come back to later.
+		}
+	}
+}
---
+++ code/UI/JumperCheckPoint.cs
@@ -0,0 +1,34 @@
+﻿
+public class JumperCheckPoint : Panel
+{
+	public static JumperCheckPoint Instance;
+	public float timesince = 0;
+	public Label jumpercp;
+	public JumperCheckPoint()
+	{
+		StyleSheet.Load( "/UI/Styles/jumperhud.scss" );
+
+		jumpercp = AddChild<Label>( "jumpercp" );
+
+		Instance = this;
+	}
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( Time.Now - timesince < 7 )
+		{
+			AddClass( "visible" );
+		}
+		else
+		{
+			RemoveClass( "visible" );
+		}
+	}
+
+	public static void ShowCheckPoint( string title )
+	{
+		Instance.jumpercp.SetText( title );
+		Instance.timesince = Time.Now;
+	}
+}
