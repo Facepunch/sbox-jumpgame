cc6ed7b832b086d91b9ae91528dc7d319c41a619
bakscratch <lt_8@hotmail.co.uk>
2022-12-26 01:00:18 +0000

    Camera scroll zoom

--- code/Player/JumperCamera.cs
+++ code/Player/JumperCamera.cs
@@ -3,8 +3,9 @@ public class JumperCamera
 {
 
 	private float distance;
-	private float targetDistance = 250f;
+	
 	private Vector3 targetPosition;
+	public float ZoomLevel { get; set; } = 250f;
 
 	public float MinDistance => 120.0f;
 	public float MaxDistance => 350.0f;
@@ -15,15 +16,18 @@ public class JumperCamera
 		if ( Game.LocalPawn is not JumperPawn pawn )
 			return;
 
+		ZoomLevel += -Input.MouseWheel * Time.Delta * 1000f;
+		ZoomLevel = ZoomLevel.Clamp( MinDistance, MaxDistance );
+
 		var distanceA = distance.LerpInverse( MinDistance, MaxDistance );
-		distance = distance.LerpTo( targetDistance, 5f * Time.Delta );
+		distance = distance.LerpTo( ZoomLevel, 5f * Time.Delta );
 		targetPosition = Vector3.Lerp( targetPosition, pawn.Position, 8f * Time.Delta );
 
 		var playerRotation = pawn.ViewAngles.ToRotation();
 
 		var height = 48f.LerpTo( 96f, distanceA );
 		var center = targetPosition + Vector3.Up * height + playerRotation.Backward * 8f;
-		var targetPos = center + playerRotation.Backward * targetDistance;
+		var targetPos = center + playerRotation.Backward * ZoomLevel;
 
 		var tr = Trace.Ray( center, targetPos )
 			.Ignore( pawn )
@@ -48,15 +52,4 @@ public class JumperCamera
 		Camera.ZNear = 6;
 		Camera.FirstPersonViewer = null;
 	}
-
-	[Event.Client.BuildInput]
-	public void BuildInput()
-	{
-		if ( Input.MouseWheel != 0 )
-		{
-			targetDistance += -Input.MouseWheel * DistanceStep;
-			targetDistance = targetDistance.Clamp( MinDistance, MaxDistance );
-		}
-	}
-
 }
