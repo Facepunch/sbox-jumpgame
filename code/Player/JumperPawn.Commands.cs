54fa29adc43c1d833ac38aa506f57eb6dec296d1
bakscratch <lt_8@hotmail.co.uk>
2023-01-19 13:11:23 +0000

    Organise some player code

---
+++ code/Player/JumperPawn.Commands.cs
@@ -0,0 +1,14 @@
+﻿using Sandbox;
+
+partial class JumperPawn
+{
+	[ConCmd.Server]
+	public static void SetPosition( Vector3 position, Angles angles )
+	{
+		var caller = ConsoleSystem.Caller;
+		if ( caller?.Pawn is not JumperPawn p ) return;
+
+		p.Position = position + Vector3.Up;
+		p.Rotation = Rotation.From( angles );
+	}
+}
--- code/Player/JumperPawn.cs
+++ code/Player/JumperPawn.cs
@@ -160,23 +160,10 @@ internal partial class JumperPawn : Sandbox.Player
 		{
 			if ( Animator is JumperAnimator animator )
 			{
-				//animator.LookAtMe = true;
-
 				SetAnimLookAt( "aim_eyes", EyePosition, LookTarget.Position + Vector3.Up  );
 				SetAnimLookAt( "aim_head", EyePosition, LookTarget.Position + Vector3.Up  );
 				SetAnimLookAt( "aim_body", EyePosition, LookTarget.Position + Vector3.Up  );
 			}
-			//CameraMode = new LookAtCamera();
-
-			//if ( CameraMode is LookAtCamera lookAtCamera )
-			//{
-			//	lookAtCamera.TargetEntity = NPCCameraTarget;
-			//	lookAtCamera.TargetOffset = new Vector3( 0, 0, 64 );
-			//	lookAtCamera.FieldOfView = 70;
-			//	lookAtCamera.MaxFov = 70;
-			//	lookAtCamera.MinFov = 50;
-			//	lookAtCamera.Origin = NPCCamera;
-			//}
 		}
 
 		if ( falleffect == null)
@@ -215,90 +202,5 @@ internal partial class JumperPawn : Sandbox.Player
 		if ( !cl.IsOwnedByLocalClient ) return;
 
 		JumperCamera?.Update();
-	}
-
-	[Event.Client.Frame]
-	private void UpdateRenderAlpha()
-	{
-
-		var dist = Camera.Position.Distance( Position );
-		var a = 1f - dist.LerpInverse( MaxRenderDistanceSelf, MaxRenderDistanceSelf * .1f );
-		a = Math.Max( a, .15f );
-		a = Sandbox.Utility.Easing.EaseOut( a );
-
-		RenderColor = RenderColor.WithAlpha( a );
-
-		foreach ( var child in Children )
-		{
-			if ( child is not ModelEntity m || !child.IsValid() ) continue;
-			m.RenderColor = m.RenderColor.WithAlpha( a );
-		}
-	}
-
-	[Event.Client.Frame]
-	private void UpdateRenderAlphaOthers()
-	{
-		if ( !Game.LocalPawn.IsValid() || Game.LocalPawn == this )
-			return;
-
-		var dist = Game.LocalPawn.Position.Distance( Position );
-		var a = 1f - dist.LerpInverse( MaxRenderDistanceOther, MaxRenderDistanceOther * .1f );
-		a = Math.Max( a, .15f );
-		a = Sandbox.Utility.Easing.EaseOut( a );
-
-		RenderColor = RenderColor.WithAlpha( a );
-
-		foreach ( var child in Children )
-		{
-			if ( child is not ModelEntity m || !child.IsValid() ) continue;
-			m.RenderColor = m.RenderColor.WithAlpha( a );
-		}
-	}
-	Particles FakeShadowParticle;
-	
-	[Event.Client.Frame]
-	void UpdatePlayerShadow()
-	{
-		FakeShadowParticle ??= Particles.Create( "particles/player/fake_shadow/fake_shadow.vpcf" );
-
-		var tr = Trace.Ray( Position, Position + Vector3.Down * 2000 )
-			.WorldOnly()
-			.Run();
-
-		FakeShadowParticle.SetPosition( 0, tr.EndPosition );
-	}
-	
-
-	TimeSince timeSinceLastFootstep = 0;
-	public override void OnAnimEventFootstep( Vector3 pos, int foot, float volume )
-	{
-		if ( LifeState != LifeState.Alive ) return;
-		if ( !Game.IsClient ) return;
-		if ( timeSinceLastFootstep < 0.2f ) return;
-
-		timeSinceLastFootstep = 0;
-
-		var footletter = foot == 0 ? "l" : "r";
-		var particle = Particles.Create( $"particles/player/footsteps/footstep_{footletter}.vpcf", pos );
-		particle.SetOrientation( 0, Transform.Rotation );
-
-		var tr = Trace.Ray( pos, pos + Vector3.Down * 20 )
-			.Radius( 1 )
-			.Ignore( this )
-			.Run();
-
-		if ( !tr.Hit ) return;
-
-		tr.Surface.DoFootstep( this, tr, foot, volume * 2 );
-	}
-
-	[ConCmd.Server]
-	public static void SetPosition( Vector3 position, Angles angles )
-	{
-		var caller = ConsoleSystem.Caller;
-		if ( caller?.Pawn is not JumperPawn p ) return;
-
-		p.Position = position + Vector3.Up;
-		p.Rotation = Rotation.From( angles );
 	}
 }
