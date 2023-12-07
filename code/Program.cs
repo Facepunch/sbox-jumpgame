415bfc6e0b9f9ccd0076f9b6f197e3abd72648cc
Tony Ferguson <devultj@gmail.com>
2023-12-07 18:36:09 +0000

    Add Program.cs

---
+++ code/Program.cs
@@ -0,0 +1,17 @@
+using Sandbox;
+
+public static class Program
+{
+	public static void Main()
+	{
+		//
+		// In the future this won't exist, we'll have an option for "default scene"
+		// and when you press play it'll load that.
+		//
+
+		GameManager.ActiveScene = new Scene();
+		GameManager.ActiveScene.LoadFromFile( "scenes/jumper.scene" );
+
+		GameManager.IsPlaying = true;
+	}
+}
