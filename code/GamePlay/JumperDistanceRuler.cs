f4cddc63c341712e558ce613bfe05c73e1053e2f
bakscratch <lt_8@hotmail.co.uk>
2024-01-18 22:18:23 +0000

    Fixed incorrect height, display other players correctly on the height bar

--- code/GamePlay/JumperDistanceRuler.cs
+++ code/GamePlay/JumperDistanceRuler.cs
@@ -4,18 +4,20 @@ public sealed class JumperDistanceRuler : Component
 {
 	[Property] public GameObject StartObject { get; set; }
 	[Property] public GameObject EndObject { get; set; }
-	public RealTimeSince SessionTimer { get; set; } = 0f;
-	public float Distance { get; private set; }
+	[Sync] public RealTimeSince SessionTimer { get; set; } = 0f;
+	[Sync] public float Distance { get; set; }
 
-
-	protected override void OnStart()
+	protected override void OnEnabled()
 	{
-		base.OnStart();
+		base.OnEnabled();
 
 		if ( StartObject is null || EndObject is null )
 			return;
 
-		var distanceBetween = Vector3.DistanceBetween( StartObject.Transform.Position.z, EndObject.Transform.Position.z );
+		var start = StartObject.Transform.LocalPosition.z;
+		var end = EndObject.Transform.LocalPosition.z;
+
+		var distanceBetween = MathF.Abs( start - end );
 
 		Distance = distanceBetween;
 	}
@@ -23,19 +25,11 @@ public sealed class JumperDistanceRuler : Component
 	protected override void DrawGizmos()
 	{
 		base.DrawGizmos();
-		var distanceBetween = Vector3.DistanceBetween( StartObject.Transform.Position.z, EndObject.Transform.Position.z );
-
-		Distance = distanceBetween;
 
 		Gizmo.Draw.Color = Color.Yellow;
 		Gizmo.Draw.LineThickness = 12;
 		Gizmo.Draw.Line( StartObject.Transform.LocalPosition, EndObject.Transform.LocalPosition );
 		Gizmo.Draw.Color = Color.Blue;
-		Gizmo.Draw.Text( $"{Distance}m", new Transform( StartObject.Transform.LocalPosition + (EndObject.Transform.LocalPosition - StartObject.Transform.LocalPosition) / 2 ),"poppins",48 );
-	}
-
-	protected override void OnUpdate()
-	{
-
+		Gizmo.Draw.Text( $"{Distance}", new Transform( StartObject.Transform.LocalPosition + (EndObject.Transform.LocalPosition - StartObject.Transform.LocalPosition) / 2 ),"poppins",48 );
 	}
 }
