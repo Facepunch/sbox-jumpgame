eb9342cbf5b71e2621b49b823b60156f0e687f18
bakscratch <lt_8@hotmail.co.uk>
2024-01-19 16:06:46 +0000

    Talking frogs (but disabled)

--- code/FunStuff/JumperNPCLooker.cs
+++ code/FunStuff/JumperNPCLooker.cs
@@ -4,17 +4,21 @@ using Sandbox.Internal;
 
 public sealed class JumperNPCLooker : Component, Component.ITriggerListener
 {
-	[Property]
-	public CitizenAnimationHelper animhelp { get; set; }
+	[Property] public CitizenAnimationHelper animhelp { get; set; }
+	[Property] public GameObject LookTarget { get; set; }
+	[Property] List<NPCTextGameResource> Resources { get; set; }
+	NPCTextGameResource NPCTextGameResource { get; set; }
 
-
-	[Property]
-	GameObject LookTarget { get; set; }
-
-	protected override void OnUpdate()
+	protected override void OnEnabled()
 	{
-		if ( LookTarget is null )
-			return;
+		base.OnEnabled();
+
+		if ( Resources != null && Resources.Count > 0 )
+		{
+			int randomIndex = new Random().Next( 0, Resources.Count );
+
+			NPCTextGameResource = Resources[randomIndex];
+		}
 	}
 
 	void ITriggerListener.OnTriggerEnter( Collider other )
@@ -26,6 +30,12 @@ public sealed class JumperNPCLooker : Component, Component.ITriggerListener
 			animhelp.EyesWeight = 1;
 			animhelp.HeadWeight = 1;
 			animhelp.BodyWeight = 1;
+			/*
+			var yapper = other.GameObject.Parent.Components.Get<JumperNPCTalker>( FindMode.EnabledInSelfAndChildren );
+			yapper.DisplayMessage( GetRandomMessage() );
+			yapper.NPCName = NPCTextGameResource.NPCName;
+			yapper.Voice = NPCTextGameResource.NPCVoice;
+			*/
 		}
 	}
 
@@ -35,8 +45,18 @@ public sealed class JumperNPCLooker : Component, Component.ITriggerListener
 		if ( other.GameObject.Tags.Has( "player" ) )
 		{
 			animhelp.LookAt = LookTarget;
-
 		}
 
+	}
+
+	private int lastMessage;
+	private string GetRandomMessage()
+	{
+		var idx = Game.Random.Int( 0, NPCTextGameResource.NPCText.Count - 1 );
+		while ( idx == lastMessage )
+			idx = Game.Random.Int( 0, NPCTextGameResource.NPCText.Count - 1 );
+
+		lastMessage = idx;
+		return string.Format( NPCTextGameResource.NPCText[idx] );
 	}
 }
---
+++ code/FunStuff/JumperNPCYapper.cs
@@ -0,0 +1,18 @@
+﻿[GameResource( "NPC TEXT", "npct", "Npc_text", Icon = "sentiment_very_satisfied", IconBgColor = "#fcba03", IconFgColor = "#363324" )]
+public class NPCTextGameResource : GameResource
+{
+	[Property]
+	[Title( "Name" )]
+	[Category( "Name" )]
+	public String NPCName { get; set; }
+
+	[Property]
+	[Title( "Text" )]
+	[Category( "Text" )]
+	public List<String> NPCText { get; set; } = new();
+
+	[Property, ResourceType( "sound" )]
+	[Title( "Voice" )]
+	[Category( "Voice" )]
+	public string NPCVoice { get; set; }
+}
