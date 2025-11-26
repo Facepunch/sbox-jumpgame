f426fb8255b07eee48dd1fa403dd16138f71372f
bakscratch <lt_8@hotmail.co.uk>
2025-11-26 06:57:37 +0000

    Camera set up

--- code/Camera/JumperCamera.cs
+++ code/Camera/JumperCamera.cs
@@ -29,7 +29,6 @@ public sealed class JumperCamera : Component
 
 		if ( CameraObject is null ) return;
 
-		//CameraObject = Scene.Components.Get<CameraComponent>( FindMode.InDescendants ).GameObject;
 		if ( CameraObject.IsValid())
 		{
 			ZoomLevel += -Input.MouseWheel.y * RealTime.Delta * 1000.0f;
@@ -37,9 +36,10 @@ public sealed class JumperCamera : Component
 
 			var distanceA = distance.LerpInverse( MinDistance, MaxDistance );
 			distance = distance.LerpTo( ZoomLevel, 5.0f * RealTime.Delta );
-			targetPosition = Vector3.Lerp( targetPosition, GameObject.Transform.Position, 8.0f * RealTime.Delta );
+			targetPosition = Vector3.Lerp( targetPosition, GameObject.WorldPosition, 8.0f * RealTime.Delta );
 
-			var pc = GameObject.Components.Get<JumperPlayerController>( FindMode.EnabledInSelfAndChildren );
+			var pc = GameObject.Components.Get<PlayerController>( FindMode.EnabledInSelfAndChildren );
+			if ( !pc.IsValid() ) return;
 			var playerRotation = pc.EyeAngles.ToRotation();
 
 			var height = 48.0f.LerpTo( 96.0f, distanceA );
@@ -56,14 +56,9 @@ public sealed class JumperCamera : Component
 				distance = Math.Min( distance, tr.Distance );
 			}
 
-			CameraObject.Transform.Position = center + playerRotation.Backward * distance;
-			CameraObject.Transform.Rotation = playerRotation;
-			CameraObject.Transform.Rotation *= Rotation.FromPitch( distanceA * 10.0f );
-
-			//Should just set on the camera 😋😋
-			//var cameracomp = CameraObject.Components.Get<CameraComponent>( FindMode.EnabledInSelfAndChildren );
-			//cameracomp.FieldOfView = 90.0f;
-			//
+			CameraObject.WorldPosition = center + playerRotation.Backward * distance;
+			CameraObject.WorldRotation = playerRotation;
+			CameraObject.WorldRotation *= Rotation.FromPitch( distanceA * 10.0f );
 		}
 	}
 	public const float MaxRenderDistanceOther = 256.0f;
@@ -72,7 +67,7 @@ public sealed class JumperCamera : Component
 	{
 		var CameraObject = Scene.GetAllComponents<CameraComponent>().Where( x => x.IsMainCamera ).FirstOrDefault();
 
-		var dist = CameraObject.Transform.Position.Distance( GameObject.Transform.Position );
+		var dist = CameraObject.WorldPosition.Distance( GameObject.WorldPosition );
 		var a = 1.0f - dist.LerpInverse( MaxRenderDistanceSelf, MaxRenderDistanceSelf * 0.1f );
 		a = Math.Max( a, .15f );
 		a = Sandbox.Utility.Easing.EaseOut( a );
@@ -82,7 +77,6 @@ public sealed class JumperCamera : Component
 		foreach ( var item in render )
 		{
 			item.Tint = item.Tint.WithAlpha( a );
-
 		}
 	}
 }
